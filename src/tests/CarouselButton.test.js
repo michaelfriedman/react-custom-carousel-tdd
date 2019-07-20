b9caba6c139c69fae1f422a62d67c313e67e1cc7
Michael Friedman <contact@michaeldavidfriedman.com>
2019-07-20 16:19:25 -0700

    :sparkles: Initial implementation of CarouselButton

--- src/tests/CarouselButton.test.js
+++ src/tests/CarouselButton.test.js
@@ -2,12 +2,31 @@ import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import CarouselButton from '../CarouselButton';
+import { executionAsyncId } from 'async_hooks';
 
 configure({ adapter: new Adapter() });
 
 describe('Carousel Button', () => {
+  const text = `Button text`;
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<CarouselButton>{text}</CarouselButton>);
+  });
   it('renders a <button>', () => {
-    const wrapper = shallow(<CarouselButton />);
+    wrapper = shallow(<CarouselButton />);
     expect(wrapper.type()).toBe('button');
+  });
+  it('passes `children through to the <button>`', () => {
+    const wrapper = shallow(<CarouselButton>{text}</CarouselButton>);
+    expect(wrapper.prop('children')).toBe(text);
+  });
+  it('passes other propr through to the <button>', () => {
+    const onClick = () => {};
+    const className = 'my-carousel-button';
+    const dataAction = 'prev';
+    wrapper.setProps({ onClick, className, 'data-action': dataAction });
+    expect(wrapper.prop('onClick')).toBe(onClick);
+    expect(wrapper.prop('className')).toBe(className);
+    expect(wrapper.prop('data-action')).toBe(dataAction);
   });
 });
