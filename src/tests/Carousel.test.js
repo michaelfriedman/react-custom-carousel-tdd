08d891227008e2cb531a39213045a3b5c5c14758
Michael Friedman <contact@michaeldavidfriedman.com>
2019-07-21 16:46:10 -0700

    :white_check_mark: Update tests for styled-components

--- src/tests/Carousel.test.js
+++ src/tests/Carousel.test.js
@@ -69,29 +69,36 @@ describe('Carousel', () => {
       wrapper.find('[data-action="next"]').simulate('click');
       expect(wrapper.state('slideIndex')).toBe(2);
     });
+  });
 
-    describe('with the first slide selected', () => {
-      it('wraps `slideIndex` to the max value when Prev is clicked', () => {
-        wrapper.setState({ slideIndex: 0 });
-        wrapper.find('[data-action="prev"]').simulate('click');
-        expect(wrapper.state('slideIndex')).toBe(slides.length - 1);
-      });
+  describe('with the first slide selected', () => {
+    it('wraps `slideIndex` to the max value when Prev is clicked', () => {
+      wrapper.setState({ slideIndex: 0 });
+      wrapper.find('[data-action="prev"]').simulate('click');
+      expect(wrapper.state('slideIndex')).toBe(slides.length - 1);
     });
+  });
 
-    describe('with the last slide selected', () => {
-      it('wraps `slideIndex` to the min value when Next is clicked', () => {
-        wrapper.setState({ slideIndex: slides.length - 1 });
-        wrapper.find('[data-action="next"]').simulate('click');
-        expect(wrapper.state('slideIndex')).toBe(0);
-      });
+  describe('with the last slide selected', () => {
+    it('wraps `slideIndex` to the min value when Next is clicked', () => {
+      wrapper.setState({ slideIndex: slides.length - 1 });
+      wrapper.find('[data-action="next"]').simulate('click');
+      expect(wrapper.state('slideIndex')).toBe(0);
     });
-    it('renders the current slide as a CarouselSlide', () => {
-      let slideProps;
-      slideProps = wrapper.find(CarouselSlide).props();
-      expect(slideProps).toEqual(slides[0]);
-      wrapper.setState({ slideIndex: 1 });
-      slideProps = wrapper.find(CarouselSlide).props();
-      expect(slideProps).toEqual(slides[1]);
+  });
+
+  it('renders the current slide as a CarouselSlide', () => {
+    let slideProps;
+    slideProps = wrapper.find(CarouselSlide).props();
+    expect(slideProps).toEqual({
+      ...CarouselSlide.defaultProps,
+      ...slides[0],
+    });
+    wrapper.setState({ slideIndex: 1 });
+    slideProps = wrapper.find(CarouselSlide).props();
+    expect(slideProps).toEqual({
+      ...CarouselSlide.defaultProps,
+      ...slides[1],
     });
   });
 });
--- src/tests/CarouselSlide.test.js
+++ src/tests/CarouselSlide.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import PropTypes from 'prop-types';
-
+import { shallow, mount } from 'enzyme';
 import CarouselSlide from '../CarouselSlide';
 
 describe('CarouselSlide', () => {
@@ -14,19 +12,23 @@ describe('CarouselSlide', () => {
       />,
     );
   });
+
   it('renders a <figure>', () => {
     expect(wrapper.type()).toBe('figure');
   });
-  it('renders an <img> and a <figcaption> as children', () => {
-    expect(wrapper.childAt(0).type()).toBe('img');
+
+  it('renders props.Img and a <figcaption> as children', () => {
+    expect(wrapper.childAt(0).type()).toBe(CarouselSlide.defaultProps.Img);
     expect(wrapper.childAt(1).type()).toBe('figcaption');
   });
+
   it('passes `imgUrl` through to the <img>', () => {
     const imgUrl = 'https://example.com/image.png';
     wrapper.setProps({ imgUrl });
-    const img = wrapper.find('img');
+    const img = wrapper.find(CarouselSlide.defaultProps.Img);
     expect(img.prop('src')).toBe(imgUrl);
   });
+
   it('uses `description` and `attribution` as the <figcaption>', () => {
     const description = 'A jaw-droppingly spectacular image';
     const attribution = 'Michael Friedman';
@@ -36,6 +38,7 @@ describe('CarouselSlide', () => {
     );
     expect(wrapper.find('figcaption strong').text()).toBe(description);
   });
+
   it('passes other props through to the <figure>', () => {
     const style = {};
     const onClick = () => {};
@@ -47,8 +50,16 @@ describe('CarouselSlide', () => {
   });
 });
 
-CarouselSlide.propTypes = {
-  imgUrl: PropTypes.string.isRequired,
-  description: PropTypes.node.isRequired,
-  attribution: PropTypes.node,
-};
+describe('Img', () => {
+  let mounted;
+  const imgUrl = 'https://example.com/default.jpg';
+
+  beforeEach(() => {
+    const Img = CarouselSlide.defaultProps.Img;
+    mounted = mount(<Img src={imgUrl} imgHeight={500} />);
+  });
+
+  it('renders an <img> with the given src', () => {
+    expect(mounted.containsMatchingElement(<img src={imgUrl} />)).toBe(true);
+  });
+});
